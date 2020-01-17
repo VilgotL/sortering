0ce9508cd7bfbb6c781851bba8a5cb82310b77da
VilgotL <54435285+VilgotL@users.noreply.github.com>
2020-01-17 12:23:38 +0100

    Create insertion.cs

---
+++ insertion.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace sortering
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> lista = new List<int>();
+
+			Random rnd = new Random();
+
+			int antal = 5000;
+
+			for (int i = 0; i < antal; i++)
+			{
+				lista.Add(rnd.Next(0, antal + 1));
+			}
+
+			int temp;
+
+			var watch = Stopwatch.StartNew();
+
+			for (int i = 1; i < lista.Count; i++)
+			{
+				int key = lista[i];
+				int j = i - 1;
+				while (j >= 0 && lista[j] > key)
+				{
+					lista[j + 1] = lista[j];
+					j = j - 1;
+				}
+				lista[j + 1] = key;
+			}
+
+			watch.Stop();
+
+			foreach (int element in lista)
+			{
+				Console.WriteLine(element);
+			}
+			Console.WriteLine();
+			Console.WriteLine(watch.ElapsedMilliseconds);
+		}
+	}
+}
